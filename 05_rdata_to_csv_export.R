62c31b106cfdd6405ec255805897afc7a88382de
petertbryant <bryant.peter@deq.state.or.us>
2016-03-02 12:56:03 -0800

    Fleshed out csv export script

--- 05_rdata_to_csv_export.R
+++ 05_rdata_to_csv_export.R
@@ -1,6 +1,15 @@
 #Designate the folder where you are getting the .Rdata file and saving the .csv
-in_out_path <- '//deqlead02/Vol_Data/Hylawoods/2015'
+in_out_path <- '//deqlead02/Vol_Data/Hylawoods/2015/'
 
 #Get the names for the .Rdata graded temperature files
-list.files(path = in_out_path, pattern = ".R[Dd]ata")
+fnames <- list.files(path = in_out_path, pattern = ".R[Dd]ata")
+
+#Set working directory
+setwd(in_out_path)
+
+#Save csv file for each fnames data frame
+for (i in 1:length(fnames)) {
+  load(fnames[i])
+  write.csv(tmp_data, file = paste0(in_out_path, fnames[i]), row.names = FALSE)
+}
 
